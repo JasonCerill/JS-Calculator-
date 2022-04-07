050ae516d4054b6ef972ab37ba65101b152a31b9
JasonCerill <101611107+JasonCerill@users.noreply.github.com>
2022-04-06 21:53:48 -0400

    Add files via upload

---
+++ JS-Calclulator/script1.js
@@ -0,0 +1,26 @@
+//function greet(name){
+    //return "Hello " + name + "!";
+//}
+
+//console.log (greet("Eric"));
+
+//let greet = function(name)
+//{
+ //   return "Hello " + name + "!";
+//}
+
+//console.log (greet ("Eric"));
+
+//function displayName(studentName){
+//    document.write(`
+//    <h2>Student List </h2>
+//    <p>Student name:  ${studentName} </p>
+//    <hr>
+//    `);
+//}
+
+function multiplyThree(number)
+{
+    return number*3
+}
+console.log(multiplyThree(8));
