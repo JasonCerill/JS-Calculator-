51eab49041be6f705ab7b1935f67db71362fc5c7
JasonCerill <jasoncer72@gmail.com>
2022-04-06 22:00:19 -0400

    function calculator

---
+++ JS-Calclulator/calculator.js
@@ -0,0 +1,7 @@
+function calculator(){
+    console.log("Calculating...");
+    let num1 = Number(prompt("enter the number")); //parse into a number
+   
+    document.getElementById("calculationZone").innerHTML=`
+    <p>${num1} * 10 = ${num1*10}</p>`;
+}
